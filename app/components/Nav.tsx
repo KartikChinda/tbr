f9cf9b1f50206bee1def245b2d4c3e9fc316a874
Kartik Chinda <kartik.chinda@gmail.com>
2024-05-13 06:27:53 +0530

    Navbar creation and color palette decision

---
+++ app/components/Nav.tsx
@@ -0,0 +1,12 @@
+
+
+const Nav = () => {
+    return (
+
+        <nav className="bg-[#9DBC98] flex justify-center items-center p-4 text-3xl w-[70%] mx-auto m-14 rounded-xl border-2 border-[#638889] text-[#EBD9B4]">
+            this is your nav
+        </nav>
+    )
+}
+
+export default Nav
